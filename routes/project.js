0f508030dc9fbdc5457539ecf38bcf725c6d0f58
vagrant <vagrant@precise32.(none)>
2014-02-19 23:50:37 +0000

    putting it together lab

--- routes/project.js
+++ routes/project.js
@@ -2,7 +2,10 @@ var models = require('../models');
 
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
-
+  models.Project
+    .find({"_id": projectID})
+    .sort('-date')
+    .exec(afterQuery);
   // query for the specific project and
   // call the following callback
 
@@ -15,6 +18,19 @@ exports.projectInfo = function(req, res) { 
 exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
+  var newProject = new models.Project({
+    "title": form_data["project_title"],
+    "date": form_data["date"],
+    "summary": form_data["summary"],
+    "image": form_data["image_url"]
+  });
+
+  newProject.save(addCallback);
+
+  function addCallback(err, projects) {
+    if(err) {console.log(err); res.send(500); }
+    res.send();//res.redirect('/');
+  }
 
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
@@ -22,6 +38,15 @@ exports.addProject = function(req, res) {
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
+  models.Project
+    .find({"_id": projectID})
+    .remove()
+    .exec(deleteCallback);
+
+  function deleteCallback(err, projects) {
+    if(err) { console.log(err); res.send(500); }
+    res.send();
+  }
 
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
